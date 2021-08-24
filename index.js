d778b50acc8b93e08191a975b88b230dd8dc3e88
Ajit <62175113+ajitpsakri@users.noreply.github.com>
2021-08-24 06:59:14 +0000

    Tinker: tried to save high score data but forgot that after refresh i will be gone.

--- index.js
+++ index.js
@@ -4,114 +4,114 @@ let chalk = require('chalk');
 
 //data
 let score = 0;
+const highScoreDB = []
 const knowledgeBase = [
-  {
-    question:"How many years was the war? ",
-    answer:"a",
-    options:{
-      one:100,
-      two:50,
-      three:87,
-      four:71
-    }
-  },
-  {
-    question:"How many episodes does Book 1-Water has? ",
-    answer:"c",
-    options:{
-      one:25,
-      two:16,
-      three:20,
-      four:18,
-    }
-  },
-  {
-    question:"what is the name of toph student 'the black one'?",
-    answer:"d",
-    options:{
-      one:"mung-dan-tae the third",
-      two:"oppa-su-fu the third",
-      three:"malinga the third",
-      four:"mucheguchelapuche the third"
-    }
-  },
-  {
-    question:"what is the name of zuko's mom? ",answer: "c",
-    options:{
-      one:"uttosama-sati",
-      two:"Mesu raion",
-      three:"Ursa",
-      four:"Shin no josei"
-    }
-  },
-  {
-    question:"How many episodes does Book 3-Fire has? ",
-    answer:"a",
-    options:{
-      one:21,
-      two:16,
-      three:70,
-      four:20
-    },
-  },
-  {
-    question:"what is the name of Aang mentor? ",
-    answer:"b",
-    options:{
-      one:"Monk Gayatori",
-      two:"Monk Gyatso",
-      three:"Monk jujutsu raka jutsu",
-      four:"Monk Gyan-hogri"
-    }
-  },
-  {
-    question:"what would you name sokka's sarcasm?",
-    answer:"d",
-    options:{
-      one:"sokmorphism",
-      two:"soka-Bumarangism",
-      three:"soka-moti-bendism",
-      four:"sokcasm"
-    }
-  },
+	{
+		question: "How many years was the war? ",
+		answer: "a",
+		options: {
+			one: 100,
+			two: 50,
+			three: 87,
+			four: 71
+		}
+	},
+	{
+		question: "How many episodes does Book 1-Water has? ",
+		answer: "c",
+		options: {
+			one: 25,
+			two: 16,
+			three: 20,
+			four: 18,
+		}
+	},
+	{
+		question: "what is the name of toph student 'the black one'?",
+		answer: "d",
+		options: {
+			one: "mung-dan-tae the third",
+			two: "oppa-su-fu the third",
+			three: "malinga the third",
+			four: "mucheguchelapuche the third"
+		}
+	},
+	{
+		question: "what is the name of zuko's mom? ", answer: "c",
+		options: {
+			one: "uttosama-sati",
+			two: "Mesu raion",
+			three: "Ursa",
+			four: "Shin no josei"
+		}
+	},
+	{
+		question: "How many episodes does Book 3-Fire has? ",
+		answer: "a",
+		options: {
+			one: 21,
+			two: 16,
+			three: 70,
+			four: 20
+		},
+	},
+	{
+		question: "what is the name of Aang mentor? ",
+		answer: "b",
+		options: {
+			one: "Monk Gayatori",
+			two: "Monk Gyatso",
+			three: "Monk jujutsu raka jutsu",
+			four: "Monk Gyan-hogri"
+		}
+	},
+	{
+		question: "what would you name sokka's sarcasm?",
+		answer: "d",
+		options: {
+			one: "sokmorphism",
+			two: "soka-Bumarangism",
+			three: "soka-moti-bendism",
+			four: "sokcasm"
+		}
+	},
 ]
-  
-    
+
+
 //functions 
-const game=()=>{
-    for(let i =0;i<knowledgeBase.length;i++){
-    //incialization
-    currentOptions = knowledgeBase[i].options
-    currentQuestion = `${i+1}) ${knowledgeBase[i].question}\na. ${currentOptions.one}\nb. ${currentOptions.two}\nc. ${currentOptions.three}\nd. ${currentOptions.four}\n`;
-    currentAnswer = knowledgeBase[i].answer;
-  
-    //branching
-    if(readlineSync.question(currentQuestion)===currentAnswer){
-      console.log(chalk.green(userName + " your currect"));
-      console.log(' ')
-      score++
-    }
-    else{
-      console.log(chalk.red(`${userName} your wrong, answer is ${currentAnswer}`))
-      console.log(' ')
-    }
-  }
+const game = () => {
+	for (let i = 0; i < knowledgeBase.length; i++) {
+		//incialization
+		currentOptions = knowledgeBase[i].options
+		currentQuestion = `${i + 1}) ${knowledgeBase[i].question}\na. ${currentOptions.one}\nb. ${currentOptions.two}\nc. ${currentOptions.three}\nd. ${currentOptions.four}\n`;
+		currentAnswer = knowledgeBase[i].answer;
+
+		//branching
+		if (readlineSync.question(currentQuestion) === currentAnswer) {
+			console.log(chalk.green(userName + " your currect"));
+			console.log(' ')
+			score++
+		}
+		else {
+			console.log(chalk.red(`${userName} your wrong, answer is ${currentAnswer}`))
+			console.log(' ')
+		}
+	}
 }
 //main function
 const userName = readlineSync.question(chalk.blueBright("What is your name? "));
 console.log(' ');
-console.log("Welcome "+chalk.bold(userName)+" to Avatar Academy, Answer the quiz and make high score to compete with you friends!");
+console.log("Welcome " + chalk.bold(userName) + " to Avatar Academy, Answer the quiz and make high score to compete with you friends!");
 console.log(' ');
 game();
 console.log(chalk.blueBright(`Score is: ${score}/7`));
 
-if(score===0){
-  console.log(chalk.blueBright("You don't know anything about Avatar  :("))
+if (score === 0) {
+	console.log(chalk.blueBright("You don't know anything about Avatar  :("))
+} else if (score > 0) {
+	if (score > 2 && score < 5) {
+		console.log(chalk.blueBright("You know about Avatar ( ╹▽╹ )"))
+	} else if (score === 7) {
+		console.log(chalk.blueBright("Your OG fan of Avatar (ﾉﾟ0ﾟ)ﾉ~"))
+	}
 }
-else if(score > 2 && score < 5){
-  console.log(chalk.blueBright("You know about Avatar ( ╹▽╹ )"))
-}else if(score === 7){
-    console.log(chalk.blueBright("Your OG fan of Avatar (ﾉﾟ0ﾟ)ﾉ~"))
-}
-
-console.log("Hey")
